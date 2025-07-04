cf82ebab3e2a4290350f2cc9944ab77bb1d15aba
David Ng <david.ng.dev@gmail.com>
2025-07-04 23:23:27 +0100

    Update Docker build: include `credentials.json` and create `user-credentials` directory in setup

--- scripts/docker/setup.sh
+++ scripts/docker/setup.sh
@@ -4,5 +4,5 @@ PYTHON_VERSION=$(cat .python-version)
 apk add gcc musl-dev linux-headers \
 python3~=$PYTHON_VERSION \
 python3-dev~=$PYTHON_VERSION
-
+mkdir user-credentials
 uv sync --no-dev --locked
